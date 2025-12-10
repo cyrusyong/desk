80edea35ec847957be606f118cd0e1d30d0435dd
Cyrus <cyong34@gatech.edu>
2025-12-10 15:08:21 -0500

    Update index.mjs

--- store-data/index.mjs
+++ store-data/index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
   // TODO implement
   const body = JSON.parse(event.body);
-  console.log("hello theree");
+  console.log("hello thereee");
 
   const response = {
     statusCode: 200,
