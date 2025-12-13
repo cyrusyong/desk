a072df3f9035fc15f4e45b3e0dc24d3441068cb0
Cyrus <cyong34@gatech.edu>
2025-12-12 20:57:46 -0500

    Sends metadata to DB with basic fields

--- store-data/index.mjs
+++ store-data/index.mjs
@@ -1,11 +1,40 @@
+import { nanoid } from "nanoid";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({
+  region: "us-east-2",
+});
+const doc_client = new DynamoDBDocumentClient(client);
+
 export const handler = async (event) => {
   // TODO implement
   const body = JSON.parse(event.body);
-  console.log("hello thereeeee");
+  // create JSON body with simpleID, fieldID, fileName, fileSize, uploadTimestamp
+  // and send it to DB
+  const simpleID = nanoid(6);
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(body),
+  const params = {
+    TableName: "S3_Metadata",
+    Item: {
+      simpleID: simpleID,
+      fieldID: body.fieldID,
+      fileName: body.fileName,
+      fileExtension: body.fileExtension,
+      fileSize: body.fileSize,
+    },
   };
-  return response;
+
+  try {
+    await doc_client.send(new PutCommand(params));
+
+    const response = {
+      statusCode: 200,
+      body: "Sent to DB",
+    };
+
+    return response;
+  } catch (e) {
+    return e;
+  }
 };
