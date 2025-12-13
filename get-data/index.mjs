13bcdd24cc335afdcf295fdce1f04b47eca26138
Cyrus <cyong34@gatech.edu>
2025-12-12 22:19:29 -0500

    Retrieves metadata and object url

--- get-data/index.mjs
+++ get-data/index.mjs
@@ -8,10 +8,24 @@ const client = new DynamoDBClient({
 const doc_client = new DynamoDBDocumentClient(client);
 
 export const handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from Lambda!"),
+  const id = event.parameters.id;
+
+  const params = {
+    TableName: "S3_Metadata",
+    Key: {
+      simpleID: id,
+    },
   };
-  return response;
+
+  try {
+    const data = await doc_client.send(new GetCommand(params));
+    const fieldID = data.Item.fieldID;
+
+    data.Item["object_url"] =
+      `https://s3.us-east-2.amazonaws.com/droplet.app/${fieldID}`;
+
+    return { status: 200, body: data };
+  } catch (e) {
+    return e;
+  }
 };
