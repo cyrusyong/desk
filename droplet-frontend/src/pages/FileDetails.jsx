2be583ec4425813d3781b563a1fd96b45740b7b3
Cyrus <cyong34@gatech.edu>
2025-12-22 19:38:03 +0900

    Create file details page

---
+++ droplet-frontend/src/pages/FileDetails.jsx
@@ -0,0 +1,11 @@
+import { Link } from "wouter";
+
+function FileDetails() {
+  return (
+    <div>
+      <h1>Hello there this is file details</h1>
+    </div>
+  );
+}
+
+export default FileDetails;
