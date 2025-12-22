44cf7b942211e32aff2d917ebc0e7c8809a4881d
Cyrus <cyong34@gatech.edu>
2025-12-22 19:38:13 +0900

    Use routes using wouter

--- droplet-frontend/src/main.jsx
+++ droplet-frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Route, Switch } from "wouter";
+import "./index.css";
+import App from "./App.jsx";
+import FileDetails from "./pages/FileDetails.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Switch>
+      <Route path="/" component={App} />
+      <Route path="/:simpleID" component={FileDetails} />
+    </Switch>
   </StrictMode>,
-)
+);
